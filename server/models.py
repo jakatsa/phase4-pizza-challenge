410654e348684b7d4f471e197faec92b67282944
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-04-17 15:04:44 +0300

    first commit

---
+++ server/models.py
@@ -0,0 +1,78 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    # add relationship
+    restaurantpizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
+
+
+    @validates('name')
+    def validate_name(self,key,  name):
+        if len(name.strip()) > 50:
+            raise ValueError("Restaurant name must be less than 50 characters")
+        return name
+    
+    # add serialization rules
+
+    def __repr__(self):
+        return f"<Restaurant {self.name}>"
+
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+
+    # add relationship
+    restaurantpizzas = db.relationship('RestaurantPizza', back_populates='pizza', lazy=True)
+
+    # add serialization rules
+
+    def __repr__(self):
+        return f"<Pizza {self.name}, {self.ingredients}>"
+
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+
+    # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='restaurantpizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurantpizzas')
+
+    # add serialization rules
+
+    # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if not 1 <= price <= 30:
+            raise ValueError("Price must be between 1 and 30")
+        return price
+
+    def __repr__(self):
+        return f"<RestaurantPizza ${self.price}>"
